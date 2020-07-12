f7d11dec7efe94352772b85f9e7f6aff5bef29a1
Manvendra Singh <mjdeora@gmail.com>
2020-07-12 22:23:59 +0530

    [Refactor]

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 /**
 index router
 */
-app.use("/", router);
+app.use('/', router);
 
 /**
 catch 404 and forward to error handler
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const controller = require("../controllers");
 
-router.get("/api/posts", controller.fetchPost);
+router.get('/api/posts', controller.fetchPost);
 
 module.exports = router;
